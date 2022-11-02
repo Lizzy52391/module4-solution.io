b7215d65a3b49a82abc7581540951d0e429605fe
Lizzy52391 <114417855+Lizzy52391@users.noreply.github.com>
2022-11-01 20:53:26 -0500

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -1,6 +1,5 @@
 (function (window) {
   var helloSpeaker = {};
-  helloSpeaker.name = "Yaakov";
   var speakWord = "Hello ";
   helloSpeaker.speak = function speak(name) {
   console.log(speakWord + name);
